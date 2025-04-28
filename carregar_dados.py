750c602598bd58f13be903ffd97a83cbdb15931c
Joao Pedro Bezerra <joaopedrobezerradev@gmail.com>
2025-04-28 16:57:42 -0300

    ...

--- carregar_dados.py
+++ carregar_dados.py
@@ -1,5 +1,14 @@
 
 import numpy as np
 
-np.loadtxt('apples_ts.csv',delimiter=",", usecols=np.arange(1, 88,1))
+dado = np.loadtxt('apples_ts.csv',delimiter=",", usecols=np.arange(1, 88,1)) # Carrega a tabela "apples_ts.csv" a partir da coluna 1 até a 87, de 1 em 1. 
+# (Também pode ser carregado via URL.)
+
+print(dado.ndim) # "ndim" usado para descobrir quantas dimensões tem os dados, nesse caso a saída será 2 (2d: Linhas e colunas).
+print(dado.size) # "size" usado para ver a quantidade de elementos contidos na tabela.
+print(dado.shape) # "Shape" mostra a quantidade de linhas e colunas contidas na tabela. 
+print(dado.T) # "T" em maisculo, realiza a transposição da tabela, transformando as linhas em colunas, facilitando a compreensão dos dados.
+
+
+
 
